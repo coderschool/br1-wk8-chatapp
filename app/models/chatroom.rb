f16ae6de6b52cdce1bb1cd0ddcea269b2fc6d659
Harley Trung <harley@coderschool.vn>
2017-08-27 11:49:53 +0800

    add bulma and style pages

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,4 +1,8 @@
 class Chatroom < ApplicationRecord
   has_many :messages, dependent: :destroy
   validates :topic, presence: true, uniqueness: true, case_sensitive: false
+
+  def messages_count
+    messages.count
+  end
 end
