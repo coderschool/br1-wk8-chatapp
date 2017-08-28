0e3f8afd4dff666201544ed61b5c8d0b13796d7d
Harley Trung <harley@coderschool.vn>
2017-08-28 11:15:54 +0700

    display flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def class_for(flash_type)
+    { success: 'is-success', error: 'is-danger', notice: 'is-warning'}[flash_type.to_sym]
+  end
+
+  # NOTE: we allow providing a custom flash so that it's easy to test
+  def flash_messages(flash)
+    flash.map do |msg_type, message|
+      content_tag(:div, class: "message #{class_for(msg_type)}") do
+        content_tag(:div, class: "message-header") do
+          (message + content_tag(:button, 'x'.html_safe, class: 'delete')).html_safe
+        end
+      end
+    end.join.html_safe
+  end
 end
